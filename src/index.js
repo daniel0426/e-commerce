8590e0c0eda8b4dd83b42c3278c748f25996f7f2
daniel0426 <angus9209@gmail.com>
2021-10-22 15:47:46 +1300

    implement commerce.js and show items, cart items

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App.js';
+import {BrowserRouter as Router} from 'react-router-dom'
 import './index.css';
-import App from './app';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+   <App/>
+   </Router>
+ ,
   document.getElementById('root')
 );
 
